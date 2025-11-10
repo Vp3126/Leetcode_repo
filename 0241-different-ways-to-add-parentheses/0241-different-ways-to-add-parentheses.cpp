afb489c68a9c1aa47af4f4ab802ee368ce302723
Vishal_Patel_26 <147002816+Vp3126@users.noreply.github.com>
2025-11-10 10:37:25 +0530

    Time: 0 ms (100%), Space: 10.4 MB (82.71%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<string, vector<int>> dp;
+
+    vector<int> diffWaysToCompute(string expression) {
+        if (dp.count(expression)) return dp[expression];
+        vector<int> res;
+        for (int i = 0; i < expression.size(); i++) {
+            char c = expression[i];
+            if (c == '+' || c == '-' || c == '*') {
+                vector<int> left = diffWaysToCompute(expression.substr(0, i));
+                vector<int> right = diffWaysToCompute(expression.substr(i + 1));
+                for (int a : left) {
+                    for (int b : right) {
+                        if (c == '+') res.push_back(a + b);
+                        else if (c == '-') res.push_back(a - b);
+                        else res.push_back(a * b);
+                    }
+                }
+            }
+        }
+        if (res.empty()) res.push_back(stoi(expression));
+        return dp[expression] = res;
+    }
+};
