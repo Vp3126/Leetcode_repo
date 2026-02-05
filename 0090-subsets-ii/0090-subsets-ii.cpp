714d08902aa0309a5469e26e9c182df3408ff3e8
Vishal Patel <147002816+Vp3126@users.noreply.github.com>
2026-02-05 11:58:31 +0530

    Time: 0 ms (100%), Space: 10.2 MB (96.68%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(int index, vector<int>& nums,
+               vector<int>& current,
+               vector<vector<int>>& ans) {
+        ans.push_back(current);
+
+        for(int i = index; i < nums.size(); i++) {
+
+            if(i > index && nums[i] == nums[i-1])
+                continue;
+
+            current.push_back(nums[i]);
+            solve(i + 1, nums, current, ans);
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> current;
+        solve(0, nums, current, ans);
+        return ans;
+    }
+};
