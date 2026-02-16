3de81be57e3220d048643ccea623745bda419283
Vishal Patel <147002816+Vp3126@users.noreply.github.com>
2026-02-16 10:39:16 +0530

    Time: 0 ms (100%), Space: 9.8 MB (9.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stk;
-
-        for (char ch : s) {
-            // If it's an opening bracket, push the corresponding closing bracket
-            if (ch == '(') {
+        stack<char>stk;
+        for(int ch:s){
+            if(ch=='('){
                 stk.push(')');
-            } else if (ch == '{') {
+            }
+            else if(ch=='{'){
                 stk.push('}');
-            } else if (ch == '[') {
+            }
+            else if(ch=='['){
                 stk.push(']');
             }
-            // If it's a closing bracket, check if it matches the top of the stack
-            else {
-                if (stk.empty() || stk.top() != ch) {
+            else{
+                if(stk.empty()||stk.top()!=ch){
                     return false;
                 }
                 stk.pop();
             }
-        }
+            
 
-        // If stack is empty, all brackets matched
+
+        }
         return stk.empty();
+
+        
     }
 };
