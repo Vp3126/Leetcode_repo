a44630800c622919c1471d5950e51681f9acfeee
Vishal Patel <147002816+Vp3126@users.noreply.github.com>
2026-02-27 09:32:27 +0530

    Time: 553 ms (43.08%), Space: 161.4 MB (50.77%) - LeetHub

--- 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.cpp
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     int minOperations(string s, int k) {
         int n = s.size();
-        int ones = 0;
 
-        for (char c : s)
-            if (c == '1')
-                ones++;
+        set<int> ts[2];
+        for (int i = 0; i <= n; i++) {
+            ts[i % 2].insert(i);
+        }
 
-        if (ones == n)
-            return 0;
+        int cnt0 = count(s.begin(), s.end(), '0');
+        ts[cnt0 % 2].erase(cnt0);
 
-        int zeros = n - ones;
+        queue<int> q;
+        q.push(cnt0);
 
-        if (k % 2 == 0 && zeros % 2 == 1)
-            return -1;
+        int ans = 0;
 
-        return (zeros + k - 1) / k;
+        while (!q.empty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int cur = q.front();
+                q.pop();
+                if (cur == 0) {
+                    return ans;
+                }
+
+                int l = cur + k - 2 * min(cur, k);
+                int r = cur + k - 2 * max(k - n + cur, 0);
+
+                auto& t = ts[l % 2];
+                auto it = t.lower_bound(l);
+
+                while (it != t.end() && *it <= r) {
+                    q.push(*it);
+                    it = t.erase(it);
+                }
+            }
+            ans++;
+        }
+
+        return -1;
     }
 };
