bcb651ad7a79e54f936446ed3b2395fa254e78f7
Vishal Patel <147002816+Vp3126@users.noreply.github.com>
2026-02-27 10:34:38 +0530

    Time: 567 ms (36.92%), Space: 161.3 MB (50.77%) - LeetHub

--- 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.cpp
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     int minOperations(string s, int k) {
         int n = s.size();
-
         set<int> ts[2];
         for (int i = 0; i <= n; i++) {
             ts[i % 2].insert(i);
         }
-
         int cnt0 = count(s.begin(), s.end(), '0');
         ts[cnt0 % 2].erase(cnt0);
-
         queue<int> q;
         q.push(cnt0);
-
         int ans = 0;
-
         while (!q.empty()) {
             for (int size = q.size(); size > 0; --size) {
                 int cur = q.front();
@@ -23,7 +18,6 @@ public:
                 if (cur == 0) {
                     return ans;
                 }
-
                 int l = cur + k - 2 * min(cur, k);
                 int r = cur + k - 2 * max(k - n + cur, 0);
 
